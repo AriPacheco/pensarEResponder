8f77c2dc331d504b1154b96858bcd3a7f1de1383
AriPacheco <arianapacheco_p@hotmail.com>
2024-12-05 21:46:42 -0300

    Faculdade Descomplica

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from "./pages/Login";
+import Menu from "./pages/menu/menu";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
